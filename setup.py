4b60541014e34b794241103a0ce447f686ca55af
Tony Hirst <tony.hirst@gmail.com>
2020-11-28 23:25:24 +0000

    Setup is still borked?

--- setup.py
+++ setup.py
@@ -19,19 +19,24 @@ setup(
     long_description=get_long_description(),
     long_description_content_type="text/markdown",
     packages=['cell_execution_status'],
-    version='0.0.4',
+    version='0.0.5',
     include_package_data=True,
     install_requires=[
         'notebook', 'jupyter_nbextensions_configurator'
     ],
-    package_data={
-        "cell_execution_status": [
-             "static/index.js",
-            "static/cell_execution_status.yaml",
-            "static/cell_status.png",
-            "static/main.css",
-            "static/README.md"
-         ],
-    },
+    data_files=[
+        # like `jupyter nbextension install --sys-prefix`
+        ("share/jupyter/nbextensions/cell_execution_status", [
+            "cell_execution_status/static/index.js",
+            "cell_execution_status/static/cell_execution_status.yaml",
+            "cell_execution_status/static/cell_status.png",
+            "cell_execution_status/static/main.css",
+            "cell_execution_status/static/README.md"
+        ]),
+        # like `jupyter nbextension enable --sys-prefix`
+        ("etc/jupyter/nbconfig/notebook.d", [
+            "jupyter-config/nbconfig/notebook.d/cell_execution_status.json"
+        ])
+    ],
     zip_safe=False
 )
