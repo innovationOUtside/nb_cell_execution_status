f859a372d40082ee60cf617e7e43ef778fb0e85a
Tony Hirst <tony.hirst@gmail.com>
2020-11-28 22:12:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,19 +8,15 @@ setuptools.setup(
     install_requires=[
         'notebook', 'jupyter_nbextensions_configurator'
     ],
-    data_files=[
-        # like `jupyter nbextension install --sys-prefix`
-        ("share/jupyter/nbextensions/cell_execution_status", [
-            "cell_execution_status/static/index.js",
-            "cell_execution_status/static/cell_execution_status.yaml",
-            "cell_execution_status/static/cell_status.png",
-            "cell_execution_status/static/main.css",
-            "cell_execution_status/static/README.md"
-        ]),
-        # like `jupyter nbextension enable --sys-prefix`
-        ("etc/jupyter/nbconfig/notebook.d", [
-            "jupyter-config/nbconfig/notebook.d/cell_execution_status.json"
-        ])
-    ],
+    include_package_data=True,
+    package_data={
+        "cell_execution_status": [
+             "static/index.js",
+            "static/cell_execution_status.yaml",
+            "static/cell_status.png",
+            "static/main.css",
+            "static/README.md"
+         ],
+    }
     zip_safe=False
 )
